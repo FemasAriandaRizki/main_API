c4e1b6d5fa61f8d30e6238e91db457f98a07e92b
femas arianda <femasariandarizki@students.undip.ac.id>
2024-11-04 21:28:11 +0700

    final commit

--- index.js
+++ index.js
@@ -9,29 +9,74 @@ const response = require("./response");
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    const sql = "SELECT * FROM makanan";
-    db.query(sql, (error, result) => {
-        // hasil data dari mysql
-        response(200, result, "get all data from makanan", res);
+    response(200, "API v1 ready to go", "SUCCESS", res);
+});
+
+app.get("/makanan", (req, res) => {
+    const sql = `SELECT * FROM makanan`;
+    db.query(sql, (err, fields) => {
+        if (err) throw err
+        response(200, fields, "makanan get list", res);
     });
 });
 
-app.get("/find", (req, res) => {
-    const sql = `SELECT nama_lengkap FROM makanan WHERE nis = ${req.query.nis}`;
-    db.query(sql, (error, result) => {
-        response(200, result, "find nama sarapan", res);
-    });
+app.get("/makanan/:nis", (req, res) => {
+    const nis = req.params.nis
+    const sql = `SELECT * FROM makanan WHERE nis = ${nis}`;
+    db.query(sql, (err, fields) => {
+        if (err) throw err
+        response(200, fields, "get detail makanan", res);
+    })
 });
 
-app.post("/login", (req, res) => {
-    console.log({ requestFromOutside: req.body });
-    res.send("login berhasil");
+app.post("/makanan", (req, res) => {
+    const { nis, nama_lengkap, jenis} = req.body
+    const sql = `INSERT INTO makanan (nis, nama_lengkap, jenis) VALUES (${nis}, '${nama_lengkap}', '${jenis}')`
+    db.query(sql, (err, fields) => {
+        if (err) response (500, "invalid", "error", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                id: fields.insertId,
+            }
+            response(200, data, "Data Added Successfully", res)
+        }
+    })
 });
 
-app.put("/username", (req, res) => {
-    console.log({ updateData: req.body });
-    res.send("update berhasil ya mas");
+app.put("/makanan", (req, res) => {
+    const { nis, namaLengkap, jenis } = req.body
+    const sql = `UPDATE makanan SET nama_lengkap = '${namaLengkap}', jenis = '${jenis}' WHERE nis = ${nis}`
+
+    db.query(sql, (err, fields) => {
+        if (err) response(500, "invalid", "error", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                message: fields.message,
+            }
+            response(200, data, "Update data succesfully", res)
+        } else {
+            response(500, "user not found", "error", res)
+        }
+    })
 });
+
+app.delete("/makanan", (req, res) => {
+    const { nis } = req.body
+    const sql = `DELETE FROM makanan WHERE nis = ${nis}`
+    db.query(sql, (err, fields) => {
+        if (err) response(500, "invalid", "error", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isDeleted: fields.affectedRows,
+            }
+            response(200, data, "Deleted Data Succesfully", res)
+        } else {
+            response(404, "User not found", "error", res)
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
